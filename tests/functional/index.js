675585406da46c8439388719fa19046726f84110
Ilche <ilcheivanovski@gmail.com>
2016-11-04 16:53:10 +0100

    Added second test

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -5,33 +5,33 @@ define(function (require) {
     registerSuite({
         name: 'index',
 
-        'greeting form': function () {
+        title_match: function () {
+            return this.remote
+                .get(require.toUrl('http://localhost:3000/#/'))
+                .getPageTitle()
+                .then(function (title) {
+                    console.log('Page loaded: ' + title);
+                    assert.equal(title, 'Angular2 Webpack Starter', 'Title should be Angular2 Webpack Starter');
+                });
+        },
+
+        'simple form': function () {
             return this.remote
                 .get(require.toUrl('http://localhost:3000/#/'))               
                 .findById('nameField')
                     .click()
                     .type('ilche@sourcico.com')
-                    .sleep(5000)                       
-                    .end()    
-                .findById('submitButton')
-                    .click()                  
-                    .end()  
-                 .findById('nameField')
-                    .click()
-                    .type('dimitar@sourcico.com')
-                    .sleep(5000)                       
+                    .sleep(5000)    
                     .end()    
                 .findById('submitButton')
                     .click()                   
-                    .end()
-                 .findById('nameField')
-                    .click()
-                    .type('wekoslav@sourcico.com')                   
-                    .sleep(5000)  
-                    .end()    
-                .findById('submitButton')
-                    .click()                
-                    .end()    
+                    .end()                
+                .findById('nameField')
+                    .getVisibleText().then(function (text) {
+                        console.log("Ilche" + text);
+                        assert.strictEqual(text, '');
+                    })
+                    .end()      
         }
     });
 
